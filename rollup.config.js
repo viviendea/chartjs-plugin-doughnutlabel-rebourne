967b0566ee0407daca7ed0b26979504020359b3c
MantasS0 <44400644+MantasS0@users.noreply.github.com>
2021-08-03 19:11:02 +0300

    Update rollup.config.js
    wip on solution for chart.js V3.5 helpers not being included in Chart Object.

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,9 @@ export default {
   name: 'DoughnutLabel',
   input: 'src/plugin.js',
   format: 'umd',
-  external: ['chart.js'],
+  external: ['chart.js', 'chart.js/helpers'],
   globals: {
     'chart.js': 'Chart',
+    'chart.js/helpers': 'Chart.helpers'
   },
 };
